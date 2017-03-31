0d446472af5127b6dbb710d983d37018ced3f098
qonfi <confidentialrendezvous@gmail.com>
2017-04-01 00:59:50 +0900

    Add files via upload

--- Components/GroundDetector.cs
+++ Components/GroundDetector.cs
@@ -1,9 +1,4 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.Networking; // (needs NetworkBehaviour)
-//using UnityEngine.UI;
-using System;
+﻿using System;
 using UnityEngine;
 
 namespace Fizix
@@ -34,8 +29,4 @@ namespace Fizix
         }
 
     }
-
-
-    
-
 }
