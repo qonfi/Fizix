8d7ce1eaaa7989518bda8310cbd0dcd1b5684a28
qonfi <confidentialrendezvous@gmail.com>
2017-04-05 21:08:47 +0900

    Add files via upload

--- Components/CameraAttacher.cs
+++ Components/CameraAttacher.cs
@@ -1,9 +1,6 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.Networking; // (needs NetworkBehaviour)
-//using UnityEngine.UI;
+﻿
 using UnityEngine;
+
 
 namespace Fizix
 {
